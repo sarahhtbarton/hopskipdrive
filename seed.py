8789dbd1356e6a4fc34d9836ba684b113f77d95c
Sarah <sarahhtbarton@gmail.com>
2021-10-19 22:03:48 +0000

    builds out seed.py

--- seed.py
+++ seed.py
@@ -7,16 +7,34 @@ import server
 import csv
 
 
-# create seed functions here
+def seed_drivers():
+    """Seed Drivers table"""
+
+    with open('data/drivers.csv', newline='') as drivers_csv:
+        drivers_reader = csv.DictReader(drivers_csv)
+        for row in drivers_reader:
+            crud.create_driver(row['full_name'], 
+                            row['place_id'])
+
+
+def seed_rides():
+    """Seed Riders table"""
+
+    with open('data/rides.csv', newline='') as rides_csv:
+        rides_reader = csv.DictReader(rides_csv)
+        for row in rides_reader:
+            crud.create_ride(row['start_address'], 
+                            row['end_address'])
 
 
 def create_example_data():
-    # include all above functions here
+    seed_drivers()
+    seed_rides()
 
 
 if __name__ == '__main__':
-    os.system('dropdb DATABASENAME-if-exists')
-    os.system('createdb DATABASENAME')
+    os.system('dropdb hopskipdrive-if-exists')
+    os.system('createdb hopskipdrive')
     model.connect_to_db(server.app)
     model.db.create_all()
     create_example_data()
