b50fb22005770ac494b134e958ee04974c214aa8
Sarah <sarahhtbarton@gmail.com>
2021-10-19 21:42:28 +0000

    builds out crud.py

--- crud.py
+++ crud.py
@@ -1,9 +1,30 @@
 """CRUD operations."""
 
-from model import db, connect_to_db, #CLASSES FROM MODEL.PY GO HERE
+from model import db, connect_to_db, Drivers, Rides
 
 
-#CRUD functions go here
+def create_driver(full_name, home_address):
+    """Create and return a new driver."""
+
+    driver = Drivers(full_name=full_name,
+                     home_address=home_address)
+    
+    db.session.add(driver)
+    db.session.commit()
+
+    return driver
+
+
+def create_ride(start_address, end_address):
+    """Create and return a new ride."""
+
+    ride = Rides(start_address=start_address,
+                     end_address=end_address)
+    
+    db.session.add(ride)
+    db.session.commit()
+
+    return ride
 
 
 if __name__ == '__main__':
