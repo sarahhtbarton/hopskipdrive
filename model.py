409867b99f6bb18b05c0bab240f752df4df4f336
Sarah <sarahhtbarton@gmail.com>
2021-10-19 21:35:19 +0000

    Builds out model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-""" Models for app. """
+""" Models for HopSkipDrive app. """
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
@@ -6,11 +6,40 @@ from datetime import date
 db = SQLAlchemy()
 
 
-#create classes here
+class Drivers(db.Model):
+    """Drivers"""
+
+    __tablename__ = 'drivers'
+
+    driver_id = db.Column(db.Integer,
+                          autoincrement=True,
+                          primary_key=True)
+    full_name = db.Column(db.String)
+    home_address = db.Column(db.String)
+
+    def __repr__(self):
+        """Show info about Drivers"""
+        return f"<Drivers driver_id={self.driver_id} home_address={self.home_address}>"
+
+
+class Rides(db.Model):
+    """Rides"""
+
+    __tablename__ = 'rides'
+
+    ride_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    start_address = db.Column(db.String)
+    end_address = db.Column(db.String)
+
+    def __repr__(self):
+        """Show info about Rides"""
+        return f"<Rides ride_id={self.ride_id} start_address={self.start_address} end_address={self.end_address}>"
 
 
 def connect_to_db(flask_app,
-                  db_uri='postgresql:///INSERTDATABASENAME',
+                  db_uri='postgresql:///hopskipdrive',
                   echo=False):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
