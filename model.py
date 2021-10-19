5d809e5a8f059456323a36c5b1ca6fdffc48f6a8
Sarah <sarahhtbarton@gmail.com>
2021-10-19 21:26:56 +0000

    creates model.py skeleton

---
+++ model.py
@@ -0,0 +1,28 @@
+""" Models for app. """
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+
+db = SQLAlchemy()
+
+
+#create classes here
+
+
+def connect_to_db(flask_app,
+                  db_uri='postgresql:///INSERTDATABASENAME',
+                  echo=False):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+
+    connect_to_db(app)
