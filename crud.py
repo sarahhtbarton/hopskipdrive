045ed19ff57bc925a8f9801ef3f452d1c6329e44
Sarah <sarahhtbarton@gmail.com>
2021-11-04 05:00:32 +0000

    adds functions to create/retrieve Commutes entries

--- crud.py
+++ crud.py
@@ -1,6 +1,8 @@
 """CRUD operations."""
 
-from model import db, connect_to_db, Drivers, Rides
+from model import Commutes, db, connect_to_db, Drivers, Rides, Commutes
+from configuration.api_key import API_KEY
+import requests
 
 
 def create_driver(full_name, email, password, home_address):
@@ -17,19 +19,26 @@ def create_driver(full_name, email, password, home_address):
     return driver
 
 
-def get_driver_address(primary_key):
-    "Return driver home address by primary key"
+def get_driver(primary_key):
+    "Return driver by primary key"
 
-    driver = db.session.query(Drivers).filter_by(driver_id=primary_key).one()
-
-    return driver.home_address
+    return db.session.query(Drivers).filter_by(driver_id=primary_key).one()
 
 
-def create_ride(start_address, end_address):
+def get_driver_by_email(email):
+    """Return driver by email"""
+
+    return Drivers.query.filter_by(email=email).one()
+
+
+def create_ride(start_address, end_address, ride_distance, ride_duration):
     """Create and return a new ride."""
 
     ride = Rides(start_address=start_address,
-                     end_address=end_address)
+                 end_address=end_address,
+                 ride_distance=ride_distance,
+                 ride_duration=ride_duration
+                )
     
     db.session.add(ride)
     db.session.commit()
@@ -41,6 +50,76 @@ def get_rides():
     """Return all rides"""
 
     return Rides.query.all()
+
+
+def create_commute(driver_id, ride_id, commute_duration, earnings):
+    """Create and return commute"""
+
+    commute = Commutes(driver_id=driver_id,
+                       ride_id=ride_id,
+                       commute_duration=commute_duration,
+                       earnings=earnings
+                      )
+    db.session.add(commute)
+    db.session.commit()
+
+    return commute
+
+
+def get_or_create_commute(ride, driver):
+    """Get commute from or create in database"""
+
+    commute_check = db.session.query(Commutes).filter(Commutes.ride_id == ride.ride_id, Commutes.driver_id == driver.driver_id).first()
+        
+    if commute_check:
+        commute = get_commute(commute_check.commute_id)
+    else:
+        commute = create_commute_logic(driver, ride)
+    
+    return commute
+
+
+def calculate_earnings(ride_distance, ride_duration):
+    """Calculate driver earnings for a given ride"""
+
+    earnings = (ride_distance * .5) + (ride_duration * 15/60)
+
+    return earnings
+
+
+def create_commute_logic(driver, ride):
+    """Computes commute for given driver and ride"""
+
+    endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+
+    parameters = {
+        'origin': f"place_id:{driver.home_address}",
+        'destination': f"place_id:{ride.start_address}",
+        'key': API_KEY
+    }
+    
+    response = requests.get(endpoint, parameters)
+    dict_response = response.json()
+
+    commute_duration = float((dict_response['routes'][0]['legs'][0]['duration']['text'])[:-5])
+    
+    earnings = calculate_earnings(ride.ride_distance, ride.ride_duration)
+
+    commute = create_commute(driver.driver_id, ride.ride_id, commute_duration, earnings)
+
+    return commute
+
+
+def get_commutes():
+    """Return all commutes"""
+
+    return Commutes.query.all()
+
+
+def get_commute(primary_key):
+    "Return commute by primary key"
+
+    return db.session.query(Commutes).filter_by(commute_id=primary_key).one()
 
 
 if __name__ == '__main__':
